4f4618d27473ea62c2ae2ce274fc5ba6c08822ca
1ndoryu <andoryyu@gmail.com>
2025-02-25 15:33:47 -0400

    *

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    server: {
+        host: "localhost",
+        port: 5176, // Fuerza el puerto correcto
+    },
     plugins: [
         laravel({
             input: "resources/js/app.jsx",
