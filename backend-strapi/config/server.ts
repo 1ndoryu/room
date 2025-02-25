0cd4772e26135116feca1e63e092da7f0f998884
1ndoryu <andoryyu@gmail.com>
2025-02-25 18:44:10 -0400

    *

--- backend-strapi/config/server.ts
+++ backend-strapi/config/server.ts
@@ -1,7 +1,14 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    url: env("URL", ""), // Usa la variable URL del .env
+    admin: {
+        path: env("ADMIN_PATH", "/admin"), // Usa ADMIN_PATH del .env
+    },
+    app: {
+        keys: env.array("APP_KEYS"),
+    },
+    webhooks: {
+        populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+    },
 });
