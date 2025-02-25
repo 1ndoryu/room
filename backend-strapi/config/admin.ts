765cbe58554e7f60cd9b7b351be89d5cb800d956
1ndoryu <andoryyu@gmail.com>
2025-02-25 19:12:11 -0400

    *

--- backend-strapi/config/admin.ts
+++ backend-strapi/config/admin.ts
@@ -1,17 +1,17 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
-    },
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
-  },
+  url: '/admin',
+  autoOpen: false,
+  watchIgnoreFiles: [
+    '**/node_modules/**',
+    '**/dist/**',
+  ],
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  serveAdminPanel: true, // Asegúrate de que esto esté en true
 });
