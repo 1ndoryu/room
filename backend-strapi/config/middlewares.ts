835f297bda8f48525092a832ab7b6f3d67d5369d
1ndoryu <andoryyu@gmail.com>
2025-02-25 19:10:51 -0400

    *

--- backend-strapi/config/middlewares.ts
+++ backend-strapi/config/middlewares.ts
@@ -6,9 +6,13 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:', 'http:', 'wss:', 'ws:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'https://wandori.us'],
-          'media-src': ["'self'", 'data:', 'blob:', 'https://wandori.us'],
+          'default-src': ["'self'", 'https:', 'http:', 'ws:', 'wss:'],
+          'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https:', 'http:'],
+          'connect-src': ["'self'", 'https:', 'http:', 'ws:', 'wss:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:', 'http:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:', 'http:'],
+          'style-src': ["'self'", "'unsafe-inline'", 'https:', 'http:'],
+          'font-src': ["'self'", 'data:', 'https:', 'http:'],
           upgradeInsecureRequests: null,
         },
       },
