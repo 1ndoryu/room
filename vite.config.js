f9ca446cb644d98aa0b5232900ab2e3e0d22931d
1ndoryu <andoryyu@gmail.com>
2025-02-25 15:12:54 -0400

    *

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,24 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    server: {
-        host: "0.0.0.0", // Permite conexiones externas
-        port: 5175,
-        hmr: {
-            host: "wandori.us",
-        },
-    },
+export default defineConfig(({ command }) => ({ // <- Usa una función
     plugins: [
         laravel({
-            input: "resources/js/app.jsx",
+            input: ['resources/js/app.jsx'],
             refresh: true,
         }),
         react(),
     ],
-});
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+    },
+    server: command === 'serve' ? { // <- Configuración condicional
+        host: '0.0.0.0',
+        port: 5175, // Elige un puerto que no esté en uso
+        hmr: {
+          host: 'wandori.us'
+        }
+    } : undefined, // <- Importante: undefined en producción
+}));
