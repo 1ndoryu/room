98eb18da0f44898e04804b211fcd91ec1aae1703
1ndoryu <andoryyu@gmail.com>
2025-02-25 19:22:48 -0400

    *

--- vite.config.js
+++ vite.config.js
@@ -4,25 +4,24 @@ import react from '@vitejs/plugin-react';
 import fs from 'fs';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        // Si necesitas SSL en desarrollo, descomenta esto:
-        // https: {
-        //     key: fs.readFileSync('/ruta/a/tu/privkey.pem'),
-        //     cert: fs.readFileSync('/ruta/a/tu/fullchain.pem'),
-        // },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    https: {
+      key: fs.readFileSync('/etc/letsencrypt/live/wandori.us/privkey.pem;'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/wandori.us/fullchain.pem'),
     },
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: [
-                'resources/routes/**',
-                'routes/**',
-                'resources/views/**',
-                '!**/backend-strapi/**',
-            ],
-        }),
-        react(),
-    ],
+  },
+  plugins: [
+    laravel({
+      input: 'resources/js/app.jsx',
+      refresh: [
+        'resources/routes/**',
+        'routes/**',
+        'resources/views/**',
+        '!**/backend-strapi/**',
+      ],
+    }),
+    react(),
+  ],
 });
