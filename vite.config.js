e3c1ba8192c346b0d801fa8a2fa2e9242ea86862
1ndoryu <andoryyu@gmail.com>
2025-02-25 15:11:15 -0400

    *

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    server: {
+        host: "0.0.0.0", // Permite conexiones externas
+        port: 5175,
+        hmr: {
+            host: "wandori.us",
+        },
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: "resources/js/app.jsx",
             refresh: true,
         }),
         react(),
