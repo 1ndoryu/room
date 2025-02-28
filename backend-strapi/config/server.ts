68ee63607673093984ab6b74be7400e81bb37987
1ndoryu <andoryyu@gmail.com>
2025-02-28 11:16:54 -0400

    *

--- backend-strapi/config/server.ts
+++ backend-strapi/config/server.ts
@@ -1,18 +1,21 @@
 export default ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  url: env("URL", "https://wandori.us/strapi"),
-  admin: {
-      auth: {
-          secret: env('ADMIN_JWT_SECRET'),
-      },
-      url: '/admin', 
-      autoOpen: false,
-  },
-  app: {
-      keys: env.array("APP_KEYS"),
-  },
-  webhooks: {
-      populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
-  },
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    url: env("URL", "https://wandori.us/strapi"),
+    admin: {
+        auth: {
+            secret: env("ADMIN_JWT_SECRET"),
+        },
+        url: "/admin",
+        autoOpen: false,
+        watchIgnoreFiles: [
+            "**/src/**", // Ignora *todo* dentro de src
+        ],
+    },
+    app: {
+        keys: env.array("APP_KEYS"),
+    },
+    webhooks: {
+        populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+    },
 });
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 import fs from "fs";
-import path from "path"; // <-- AÑADE ESTA LÍNEA
+import path from "path";
 
 const host =
     process.env.APP_ENV === "production"
-        ? "0.0.0.0"
+        ? "0.0.0.0" // Cambia esto a "wandori.us" si es necesario
         : process.env.APP_HOST || "localhost";
 const useHttps = process.env.APP_ENV === "production";
 
@@ -15,8 +15,14 @@ let serverConfig = {
     host,
     port: 5173,
     hmr: {
-        host,
-        clientPort: 5173,
+        host, // Esto también podría ser "wandori.us" en producción.
+        clientPort: 5173, // Considera usar 443 en producción si usas HTTPS
+    },
+    cors: {
+        origin: "https://wandori.us", //  <--  ¡AÑADE ESTO!  Solo tu dominio.
+        // methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'], // Opcional: métodos permitidos
+        // allowedHeaders: ['Content-Type', 'Authorization'], // Opcional: cabeceras permitidas
+        // credentials: true,  // Opcional: si necesitas enviar cookies/credenciales
     },
 };
 
