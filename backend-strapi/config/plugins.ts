791b620c2501e963bab50b7f70582c6e50ffe0d8
1ndoryu <andoryyu@gmail.com>
2025-03-01 22:38:48 +0100

    Sync: Actualizar desde main

--- backend-strapi/config/plugins.ts
+++
@@ -1,9 +0,0 @@
-export default ({ env }: { env: any }) => ({
-    "users-permissions": {
-        config: {
-            register: {
-                allowedFields: ["name", "username", "email", "password"],
-            },
-        },
-    },
-});
