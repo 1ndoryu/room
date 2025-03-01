56351e0f686064527bb90d5687097886274d67a8
1ndoryu <andoryyu@gmail.com>
2025-03-01 16:57:07 -0400

    *

--- backend-strapi/config/server.ts
+++ backend-strapi/config/server.ts
@@ -1,18 +1,19 @@
 export default ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  url: env("URL", "https://wandori.us/strapi"),
-  admin: {
-      auth: {
-          secret: env('ADMIN_JWT_SECRET'),
-      },
-      url: '/admin', 
-      autoOpen: false,
-  },
-  app: {
-      keys: env.array("APP_KEYS"),
-  },
-  webhooks: {
-      populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
-  },
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    app: {
+        keys: env.array("APP_KEYS"),
+    },
+    admin: {
+        path: env("ADMIN_PATH", "/admin"),
+        auth: {
+            secret: env("ADMIN_JWT_SECRET"),
+        },
+        // URL pública completa del panel de administración
+        url: "https://wandori.us/admin",
+        autoOpen: false,
+    },
+    webhooks: {
+        populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+    },
 });
--- backend-strapi/helpers/logger.d.ts
+++
@@ -1,5 +0,0 @@
-// helpers/logger.d.ts
-import { Logger } from 'winston'; // Importa los tipos de winston
-
-declare const logger: Logger; // Declara que 'logger' es una instancia de Logger
-export default logger;        // Y exporta
